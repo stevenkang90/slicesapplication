fa664a06cdc63285f4cc4ca906d8e39880426f6e
ska23 <stevenkang90@gmail.com>
2018-05-11 08:53:21 +0100

    first commit

---
+++ app/src/main/java/com/example/ska23/slicesapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ska23.slicesapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
